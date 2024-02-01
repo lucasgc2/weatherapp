2ed7fb3801b2ede09d3d96a63b2562e108a8a015
lucasgc <80920358+lucasgc2@users.noreply.github.com>
2024-02-01 21:57:41 +0100

    Update weather.js

--- Web/weather.js
+++ Web/weather.js
@@ -1,3 +1,3 @@
-// https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=271bb59eea50683def1b4a90896beb28&units=%22metric%22
+// https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}&units=%22metric%22
 
-const apiKey = '271bb59eea50683def1b4a90896beb28';
+const apiKey = '';
